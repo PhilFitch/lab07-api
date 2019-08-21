d8b3da7dcdd8f202555fd9999ab5698e0af5b835
Angela Vawser <amvawser@gmail.com>
2019-08-21 10:17:02 -0700

    using API route for weather and maps?

--- server.js
+++ server.js
@@ -18,61 +18,34 @@ app.use(cors());
 // Map (Google) API Route
 
 app.get('/location', (request, response) => {
-    try {
-        const location = request.query.location;
-        const result = getLatLng(location);
-        response.status(200).json(result);
-    } catch(err) {
-        response.status(500).send('Sorry, something went wrong. Please try again');
-    }
+    const search = request.query.search;
+    mapsApi.getLocation(search)
+        .then(location => {
+            response.json(location);
+        })
+        .catch(err => {
+            response.status(500).json({
+                error: err.message || err
+            });
+        });
 });
-
-const geoData = require('./data/geo.json');
-
-function getLatLng(/*location*/) {
-    //api call will go here
-    return toLocation(geoData);
-}
-
-function toLocation(/*geoData*/) {
-    const firstResult = geoData.results[0];
-    const geometry = firstResult.geometry;
-    
-    return {
-        formatted_query: firstResult.formatted_address,
-        latitude: geometry.location.lat,
-        longitude: geometry.location.lng
-    };
-}
 
 // Weather (darksky) API Route
 
 app.get('/weather', (request, response) => {
-    try {
-        const weather = request.query.weather;
-        const result = getForecast(weather);
-        response.status(200).json(result);
-    }
+    const latitude = request.query.latitude;
+    const longitude = request.query.longitude;
 
-    catch(err) {
-        // TODO: make an object and send via .json...
-        response.status(500).send('Sorry something went wrong, please try again');
-    }
-
+    weatherApi.getForecast(latitude, longitude)
+        .then(forecast => {
+            response.json(forecast);
+        })
+        .catch(err => {
+            response.status(500).json({
+                error: err.message || err
+            });
+        });
 });
-
-function getForecast() {
-    const forecast = [];
-
-    for(let i = 0; i < weatherApi.daily.data.length; i++) {
-        const dailyForecast = {
-            forecast: weatherApi.daily.data[i].summary,
-            time: weatherApi.daily.data[i].time
-        };
-        forecast.push(dailyForecast);
-    }
-    return forecast;
-}
 
 app.listen(PORT, () => {
     console.log('server running on PORT', PORT);
