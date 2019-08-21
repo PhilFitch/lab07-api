e881f306a2ae37fcaca2564a99cba500007abcfd
Phil Fitch <mostreluctantly@gmail.com>
2019-08-20 17:30:40 -0700

    setup weather-api.js

---
+++ server.js
@@ -0,0 +1,96 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const weatherApi = require('./lib/weather-api');
+const mapsApi = require('./lib/maps-api');
+const morgan = require('morgan');
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(morgan('dev'));
+
+// geoData (map) set up
+
+app.get('/location', (request, response) => {
+    try {
+        const location = request.query.location;
+        const result = getLatLng(location);
+        response.status(200).json(result);
+    } catch(err) {
+        response.status(500).send('Sorry, something went wrong. Please try again');
+    }
+});
+
+const geoData = require('./data/geo.json');
+
+function getLatLng(/*location*/) {
+    //api call will go here
+    return toLocation(geoData);
+}
+
+function toLocation(/*geoData*/) {
+    const firstResult = geoData.results[0];
+    const geometry = firstResult.geometry;
+    
+    return {
+        formatted_query: firstResult.formatted_address,
+        latitude: geometry.location.lat,
+        longitude: geometry.location.lng
+    };
+}
+
+// darksky (weather) setup
+
+app.get('/weather', (request, response) => {
+    try {
+        const location = request.query.location;
+        const result = getForecastTime(location);
+        response.status(200).json(result);
+    } catch(err) {
+        response.status(500).send('Sorry, something went wrong. Please try again');
+    }
+});
+
+const darkSky = require('./data/darksky.json');
+
+function getForecastTime(/*location*/) {
+    //api call will go here
+    return toWeather(darkSky);
+}
+
+function toWeather(/*darkSky*/) {
+    const firstResult = geoData.results[0];
+    
+    return [{
+        formatted_query: firstResult.formatted_address,
+        forecast: darkSky.daily.data[0].summary,
+        time: darkSky.daily.data[0].time
+    },
+    {
+        formatted_query: firstResult.formatted_address,
+        forecast: darkSky.daily.data[1].summary,
+        time: darkSky.daily.data[1].time
+    },
+    {
+        formatted_query: firstResult.formatted_address,
+        forecast: darkSky.daily.data[2].summary,
+        time: darkSky.daily.data[2].time
+    },
+    {
+        formatted_query: firstResult.formatted_address,
+        forecast: darkSky.daily.data[3].summary,
+        time: darkSky.daily.data[3].time
+    },
+    {
+        formatted_query: firstResult.formatted_address,
+        forecast: darkSky.daily.data[4].summary,
+        time: darkSky.daily.data[4].time
+    }
+    ];
+}
+
+app.listen(PORT, () => {
+    console.log('server running on PORT', PORT);
+});
